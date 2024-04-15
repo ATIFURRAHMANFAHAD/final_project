53b78561fb5e8481068ff0591f001a344b98791d
ATIFURRAHMANFAHAD <arfahad@myseneca.ca>
2024-04-15 00:19:48 -0400

    Version 2

---
+++ Version2/manipulating.h
@@ -0,0 +1,55 @@
+/*
+General Information on
+===========================
+Source file Name: manipulating.h
+
+MANIPULATING MODULE HEADER
+This is the header file of string manipulating operation
+
+The main purpose of the file 
+1. Include Macro
+2. Include necessary C libraries.
+3. Include user-defined libraries
+
+Credit:
+Author 			: Atifur Fahad
+Group Name		: Bb 4
+Group Members	: Students of Seneca College
+Course 			: CPR 101
+
+Supervisor		: Professor Raymond Wong
+
+Revision		: 2.0.5
+Version 		: 2
+
+License 		: Not Defined 
+
+Release date	: 16 April 2024
+Support			: mrkarim6@myseneca.ca
+*/
+
+// Start program
+
+/* 
+ * If the macro is defined or not 
+ * Define it if not defined already
+ */
+#ifndef _MANIPULATING_H_
+//
+// Define the macro _MANIPULATING_H_
+#define _MANIPULATING_H_
+//
+// Include the stander input and output C libraries.
+#include <stdio.h>
+//
+// Include the stander string C libraries.
+#include <string.h>
+//
+// Declare the prototype of fx manipulating.
+// The function does not take or return any parameters.
+void manipulating(void);
+
+// End the if conditoin
+#endif
+
+// End program
