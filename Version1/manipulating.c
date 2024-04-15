89d6088ac8b976b6ac7a3286825d3a7502a83d91
ATIFURRAHMANFAHAD <arfahad@myseneca.ca>
2024-04-15 00:18:01 -0400

    Version 1

---
+++ Version1/manipulating.c
@@ -0,0 +1,111 @@
+/*
+General Information on
+===========================
+Source file Name: manipulating.c
+
+ MANIPULATING SOURCE
+This is the source file of string manipulating operation
+
+The main purpose of the file 
+1. Include Macro
+2. Include user-defined libraries
+3. Perform the actual logic of operations. 
+
+Credit:
+Author          : Atifur Fahad
+Group Name		: Bb 4
+Group Members	: Students of Seneca College
+Course 			: CPR 101
+
+Supervisor		: Professor Raymond Wong
+
+Revision		: 1.0.5
+Version 		: 1
+
+License 		: Not Defined 
+
+Release date	: 16 April 2024
+Support			: mrkarim6@myseneca.ca
+*/
+
+// Start Program 
+
+/*
+ * Tells the compiler not to issue warnings when these unsafe functions are used. A quick fix
+ * to suppress warnings without addressing the underlying issue of using unsafe functions.
+ */
+#define _CRT_SECURE_NO_WARNINGS
+
+//
+// Define a macro for the input buffer array size.
+#define BUFFER_SIZE 80
+
+//
+// Include the user-defined header file.
+#include "manipulating.h"
+
+/*
+ * Method Purpose     : The function finds a character opsition in a string.
+ * Input peerameter : None 
+ * Output peerameter: None
+ */
+void manipulating(void)
+{
+	//
+	// Print the boot message of the program. 
+	printf("*** Start of Concatenation String Demo ***\n");
+	
+	//
+	// Declare a variable in the chacater array with the length of the double buffer size.			
+	char string1[BUFFER_SIZE * 2];
+	//
+	// Declare a variable in the chacater array with the length of the buffer size.		
+	char string2[BUFFER_SIZE];
+	
+	//
+	// Used "do while" to ensure at least one input by the user
+	//	
+	do
+	{
+		// Display mesage to accept an not empy strig
+		printf("Type the 1st string (q - to quit):\n");
+		
+		//
+		// Accept user input with length of BUFFER_SIZE		
+		fgets(string1, BUFFER_SIZE, stdin);
+		
+		//
+		// Define the last buffer NULL value 
+		string1[strlen(string1) - 1] = '\0';
+		
+		//
+		// Check and allow users if they are not entered by zero
+		if((strcmp(string1,"q") != 0))
+		{
+			//
+			// Accept user input for 2nd string with length of BUFFER_SIZE		
+			printf("Type the 2nd string:\n");
+			fgets(string2,BUFFER_SIZE, stdin);
+			
+			//
+			// Define the last buffer NULL value 
+			string2[strlen(string2) - 1] = '\0';
+			
+			//
+			// Concatenating two string
+			strcat(string1, string2);
+			
+			//
+			// Print the result
+			printf("Concatenation string is \' %s \'\n", string1);
+		}
+	}
+	// Continue the program until the user presses 'q'
+	while(strcmp(string1, "q") != 0);
+	
+	//
+	// Print the end message of the program. 
+	printf("*** End of Concatenating String Demo ***\n\n");
+}
+
+// End Program 
